216a368c620887a758cb47aafff9157e466806e5
4maumau <mauriciowgarcia@gmail.com>
2020-10-27 10:57:37 -0300

    we workinnn

---
+++ ProjetoPipo/Assets/Scripts/Objetos/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Cinemachine;
+using UnityEngine;
+using static GameEvents;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int value = 1;
+    [SerializeField] private GameObject destroyEffect;
+
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            TriggerCoinGet(value);
+
+            FindObjectOfType<AudioManager>().PlaySound("ItemPickup");
+            GameObject CoinPickupVFX = Instantiate(destroyEffect, this.transform.position, Quaternion.identity);
+            Destroy(CoinPickupVFX, 1f);
+            Destroy(gameObject);
+        }
+    }
+}
